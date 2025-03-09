c871226219ba3e3f6cba7d54db54fe6431a8c61f
juaxxxx <164902625+juaxxxx@users.noreply.github.com>
2025-03-09 22:17:23 +0900

    Section3Fin.

--- Source/WarriorCombat/Private/Items/Weapons/WarriorHeroWeapon.cpp
+++ Source/WarriorCombat/Private/Items/Weapons/WarriorHeroWeapon.cpp
@@ -3,3 +3,13 @@
 
 #include "Items/Weapons/WarriorHeroWeapon.h"
 
+void AWarriorHeroWeapon::AssignGrantedAbilitySpecHandles(const TArray<FGameplayAbilitySpecHandle>& InSpecHandles)
+{
+	GrantedAbilitySpecHandles = InSpecHandles;
+
+}
+
+TArray<FGameplayAbilitySpecHandle> AWarriorHeroWeapon::GetGrantedAbilitySpecHandles() const
+{
+	return GrantedAbilitySpecHandles;
+}
--- Source/WarriorCombat/Public/AbilitySystem/WarriorAbilitySystemComponent.h
+++ Source/WarriorCombat/Public/AbilitySystem/WarriorAbilitySystemComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "WarriorTypes/WarriorStructTypes.h"
 #include "WarriorAbilitySystemComponent.generated.h"
 
 /**
@@ -17,4 +18,14 @@ class WARRIORCOMBAT_API UWarriorAbilitySystemComponent : public UAbilitySystemCo
 public:
 	void OnAbilityInputPressed(const FGameplayTag& InInputTag);
 	void OnAbilityInputReleased(const FGameplayTag& InInputTag);
+
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Ability", meta = (ApplyLevel = "1"))
+	void GrantHeroWeaponAbilities(const TArray<FWarriorHeroAbilitySet>& InDefaultWeaponAbilities, int32 ApplyLevel, TArray<FGameplayAbilitySpecHandle>& OutGrantedAbilitySpecHandles);
+
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Ability")
+	void RemovedGrantedHeroWeaponAbilities(UPARAM(ref) TArray<FGameplayAbilitySpecHandle>& InSpecHandlesToRemove);
 };
+
+
+
+
